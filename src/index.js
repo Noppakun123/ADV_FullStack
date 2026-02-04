191fdb8e9db82792048f9f6ab73d8a4bb9484f70
Noppakun123 <nguay789@gmail.com>
2026-02-04 11:27:22 +0700

    update greeting message in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Noppakun World!");
+  res.send("Hello, Noppakun World! hnklcnkjcsdnlclcjkcejkce");
 });
 
 app.listen(port, () => {
